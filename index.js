1311a8d0305ac677683c7ede7c03d1253771214b
reys <reysluisa0@gmail.com>
2023-12-07 15:20:32 +0200

    ch2

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('A Monk in Cloud');
   res.end();
 });
 
-server.listen(80, () => {
-  console.log('Server is running on port 80');
+const PORT = 3000; // Use a higher port number
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
